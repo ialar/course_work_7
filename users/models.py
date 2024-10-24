18e696388054220bf76f1d0b036bfa57c65a8c0f
Igor Larionov <jedialar@gmail.com>
2024-10-24 17:24:26 +0300

    edit model User

--- users/models.py
+++ users/models.py
@@ -1,5 +1,18 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    username = None
+    email = models.EmailField(unique=True, verbose_name="Почта")
+    tg_chat_id = models.CharField(max_length=100, verbose_name='Телеграм chat_id', null=True, blank=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return f'{self.email}'
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
