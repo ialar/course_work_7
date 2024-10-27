657a6e7a4f4896fa72f6d48c173bf108c23547e6
Igor Larionov <jedialar@gmail.com>
2024-10-27 22:01:53 +0300

    edit serializers.py and validators.py

--- habits/serializers.py
+++ habits/serializers.py
@@ -1,11 +1,16 @@
 from rest_framework import serializers
 
 from habits.models import Habit
-from habits.validators import HabitValidator
+from habits.validators import DurationValidator, IsRelatedEnjoyableValidator, IsEnjoyableRelatedValidator, \
+    FrequencyValidator, RewardOrRelatedValidator
 
 
 class HabitSerializer(serializers.ModelSerializer):
     class Meta:
         model = Habit
         fields = "__all__"
-        validators = [HabitValidator]
+        validators = [RewardOrRelatedValidator(field="reward"),
+                      DurationValidator(field="duration"),
+                      IsRelatedEnjoyableValidator(field="related"),
+                      IsEnjoyableRelatedValidator(field="is_enjoyable"),
+                      FrequencyValidator(field="frequency")]
--- habits/validators.py
+++ habits/validators.py
@@ -1,19 +1,50 @@
 from rest_framework.serializers import ValidationError
 
 
-class HabitValidator:
+class RewardOrRelatedValidator:
     def __init__(self, field):
         self.field = field
 
     def __call__(self, habit):
-        if habit.get("related") and habit.get("reward"):
-            raise ValidationError("Может быть одно из двух: либо связанная привычка, либо вознаграждение.")
+        reward, related = habit.get("reward"), habit.get("related")
+        if reward and related:
+            raise ValidationError("За выполнение может быть либо вознаграждение, либо связанная привычка.")
+
+
+class DurationValidator:
+    def __init__(self, field):
+        self.field = field
+
+    def __call__(self, habit):
         if habit.get("duration") and habit.get("duration") > 120:
             raise ValidationError("Время выполнения привычки должно быть не больше 2 минут.")
+
+
+class IsRelatedEnjoyableValidator:
+    def __init__(self, field):
+        self.field = field
+
+    def __call__(self, habit):
         if habit.get("related"):
             if not habit.get("related").is_enjoyable:
                 raise ValidationError("Связанная привычка может быть только приятной.")
-        if habit.get("is_enjoyable").reward or habit.get("is_enjoyable").related:
-            raise ValidationError("У приятной привычки не может быть вознаграждения или связанной привычки.")
+
+
+class IsEnjoyableRelatedValidator:
+    def __init__(self, field):
+        self.field = field
+
+    def __call__(self, habit):
+        reward, related = habit.get("reward"), habit.get("related")
+        if habit.get("is_enjoyable"):
+            if reward or related:
+                raise ValidationError("У приятной привычки не может быть вознаграждения или связанной привычки.")
+
+
+class FrequencyValidator:
+    def __init__(self, field):
+        self.field = field
+
+    def __call__(self, habit):
         if habit.get("frequency") < 1:
             raise ValidationError("За неделю необходимо выполнить привычку хотя бы один раз.")
