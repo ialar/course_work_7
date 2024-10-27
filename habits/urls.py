9dd3b83e724be9d429c4fc1cc8529d79292313bf
Igor Larionov <jedialar@gmail.com>
2024-10-28 00:55:28 +0300

    add jwt and public habits list

--- habits/urls.py
+++ habits/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from habits.apps import HabitsConfig
 from habits.views import HabitCreateAPIView, HabitRetrieveAPIView, HabitListAPIView, HabitUpdateAPIView, \
-    HabitDestroyAPIView
+    HabitDestroyAPIView, PublicHabitListAPIView
 
 app_name = HabitsConfig.name
 urlpatterns = [path("habits/create/", HabitCreateAPIView.as_view(), name="habits-create"),
                path("habits/<int:pk>/", HabitRetrieveAPIView.as_view(), name="habits-retrieve"),
                path("habits/", HabitListAPIView.as_view(), name="habits-list"),
                path("habits/<int:pk>/update/", HabitUpdateAPIView.as_view(), name="habits-update"),
-               path("habits/<int:pk>/delete/", HabitDestroyAPIView.as_view(), name="habits-delete")]
+               path("habits/<int:pk>/delete/", HabitDestroyAPIView.as_view(), name="habits-delete"),
+               path("habits/public/", PublicHabitListAPIView.as_view(), name="public-habits-list")]
--- habits/views.py
+++ habits/views.py
@@ -1,5 +1,5 @@
 from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveAPIView, UpdateAPIView, DestroyAPIView
-# from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
 
 from habits.models import Habit
 from habits.paginators import CustomPagination
@@ -8,7 +8,12 @@ from habits.serializers import HabitSerializer
 
 class HabitCreateAPIView(CreateAPIView):
     serializer_class = HabitSerializer
-    # permission_classes = (IsAuthenticated,)
+    permission_classes = (IsAuthenticated,)
+
+    def perform_create(self, serializer):
+        habit = serializer.save()
+        habit.owner = self.request.user
+        habit.save()
 
 
 class HabitRetrieveAPIView(RetrieveAPIView):
@@ -21,6 +26,10 @@ class HabitListAPIView(ListAPIView):
     serializer_class = HabitSerializer
     pagination_class = CustomPagination
 
+    def get_queryset(self):
+        queryset = self.queryset.filter(owner=self.request.user)
+        return queryset
+
 
 class HabitUpdateAPIView(UpdateAPIView):
     queryset = Habit.objects.all()
@@ -29,3 +38,10 @@ class HabitUpdateAPIView(UpdateAPIView):
 
 class HabitDestroyAPIView(DestroyAPIView):
     queryset = Habit.objects.all()
+
+
+class PublicHabitListAPIView(ListAPIView):
+    serializer_class = HabitSerializer
+    queryset = Habit.objects.filter(is_public=True)
+    permission_classes = (AllowAny,)
+    pagination_class = CustomPagination
