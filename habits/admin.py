bc59d3b0f697bf9dd81129d6dec26cd03d6a32e0
Igor Larionov <jedialar@gmail.com>
2024-10-29 00:34:17 +0300

    edit admin.py

--- habits/admin.py
+++ habits/admin.py
@@ -7,4 +7,4 @@ from habits.models import Habit
 class UserAdmin(admin.ModelAdmin):
     list_display = ("id", "owner", "place", "time", "action", "duration",
                     "reward", "related", "frequency", "is_enjoyable", "is_public")
-    list_filter = ("id", "owner", "is_public", "is_enjoyable")
+    # list_filter = ("id", "owner", "is_public", "is_enjoyable")
