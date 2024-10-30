6e2e85ab426c614888cc4c7826ca6ed5369c4cab
Igor Larionov <jedialar@gmail.com>
2024-10-30 23:46:26 +0300

    add tests

--- habits/tests.py
+++ habits/tests.py
@@ -1,3 +1,68 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+from habits.models import Habit
+from users.models import User
+
+
+class HabitTestCase(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create(email="test@test.com")
+        self.client.force_authenticate(user=self.user)
+        self.habit = Habit.objects.create(owner=self.user, place="test", time="00:00:00", action="test",
+                                          is_enjoyable="False", is_public="True")
+
+    def test_habit_retrieve(self):
+        url = reverse("habits:habits-retrieve", args=(self.habit.pk,))
+        response = self.client.get(url)
+        data = response.json()
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(data.get("action"), self.habit.action)
+
+    def test_habit_create(self):
+        url = reverse("habits:habits-create")
+        data = {"place": "test0", "time": "00:00:00", "action": "test0"}
+        response = self.client.post(url, data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Habit.objects.all().count(), 2)
+
+    def test_habit_update(self):
+        url = reverse("habits:habits-update", args=(self.habit.pk,))
+        data = {"time": "00:00:01"}
+        response = self.client.patch(url, data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(data.get("time"), "00:00:01")
+
+    def test_habit_delete(self):
+        url = reverse("habits:habits-delete", args=(self.habit.pk,))
+        response = self.client.delete(url)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(Habit.objects.all().count(), 0)
+
+    def test_habits_list(self):
+        url = reverse("habits:habits-list")
+        response = self.client.get(url)
+        data = response.json()
+        result = {"count": 1,
+                  "next": None,
+                  "previous": None,
+                  "results": [{"id": self.habit.pk,
+                               "place": self.habit.place,
+                               "time": self.habit.time,
+                               "action": self.habit.action,
+                               "duration": None,
+                               "reward": None,
+                               "frequency": self.habit.frequency,
+                               "is_enjoyable": False,
+                               "is_public": True,
+                               "owner": self.habit.owner.pk,
+                               "related": None}]}
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(data, result)
+
+    def test_public_habits_list(self):
+        url = reverse("habits:public-habits-list")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertTrue(len(response.data) > 0)
